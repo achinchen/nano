8cc07ffa2f8e1a9cbd86fb406bbc61c9f604300b
Chin <greeting.to.chin@gmail.com>
2023-07-09 16:09:38 +0800

    chore(backend): add vercel config

--- services/backend/src/app.ts
+++ services/backend/src/app.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send({ message: 'Welcome to Vercel!' });
+});
+
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to backend!' });
 });
