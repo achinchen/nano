d05e58171a3e1616c8bcc1e017c2bba988363ad1
Chin <greeting.to.chin@gmail.com>
2023-07-26 23:00:25 +0800

    chore(backend): adjust logger

--- services/backend/src/main.ts
+++ services/backend/src/main.ts
@@ -6,7 +6,7 @@ async function main() {
     await dataSource.initialize();
     const port = process.env.PORT || 3333;
     const server = app.listen(port, () => {
-      console.log(`Listening at http://localhost:${port}/api`);
+      console.log(`Listening at http://localhost:${port}/`);
     });
     server.on('error', console.error);
   } catch (error) {
