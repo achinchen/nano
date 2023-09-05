f3bbc8292b4fa20c4f81e32ed168de6c89bfacea
Chin <greeting.to.chin@gmail.com>
2023-09-05 22:39:09 +0800

    feat(frontend): setup icon

---
+++ services/frontend/app/components/Icon/index.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta } from '@storybook/react';
+import { Icon } from '.';
+
+const Story: Meta<typeof Icon> = {
+  component: Icon,
+  title: 'Icon',
+};
+export default Story;
+
+export const Default = {
+  args: {
+    icon: 'i-svg-spinners-180-ring-with-bg',
+  },
+};
--- services/frontend/jest.config.ts
+++ services/frontend/jest.config.ts
@@ -8,4 +8,7 @@ export default {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: './coverage',
+  setupFilesAfterEnv: [
+    "@testing-library/jest-dom"
+  ],
 };
