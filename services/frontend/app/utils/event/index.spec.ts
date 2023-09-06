b939f7154e8d65351475b46e074282604a18217a
Chin <greeting.to.chin@gmail.com>
2023-09-06 10:25:38 +0800

    feat(frontend): add event

---
+++ services/frontend/app/utils/event/index.spec.ts
@@ -0,0 +1,79 @@
+import { createEventEmitter } from '.';
+
+jest.mock('.', () => ({
+  ...jest.requireActual('.'),
+  initLogger: () => ({
+    info: jest.fn(),
+  }),
+}));
+
+describe('EventEmitter', () => {
+  const mockService = 'mock-service';
+  const mockEventName = 'mock:custom:action';
+  const mockListener = jest.fn();
+
+  Object.defineProperty(global, 'document', {
+    value: {
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    },
+    writable: true,
+  });
+
+  console.info = () => {
+    /* */
+  };
+
+  const setupEventEmitter = () => {
+    const eventEmitter = createEventEmitter(mockService);
+    eventEmitter.subscribe(mockEventName, mockListener);
+
+    return eventEmitter;
+  };
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('EventEmitter.subscribe', () => {
+    it('should add a message event listener to document', () => {
+      setupEventEmitter();
+
+      expect(document.addEventListener).toHaveBeenCalled();
+    });
+
+    it('should call listener', () => {
+      setupEventEmitter();
+
+      const mockCustomEvent = new CustomEvent(mockEventName, {
+        detail: 'test-data',
+      });
+      (document.addEventListener as jest.Mock).mock.calls[0][1](
+        mockCustomEvent
+      );
+      expect(mockListener).toHaveBeenCalledWith(mockCustomEvent.detail);
+    });
+  });
+
+  describe('EventEmitter.emit', () => {
+    it('should call document.dispatchEvent with payload', () => {
+      const eventEmitter = createEventEmitter(mockService);
+      const mockPayload = 'test-payload';
+      eventEmitter.emit(mockEventName, mockPayload);
+      const mockArgument = new CustomEvent(mockEventName, {
+        detail: mockPayload,
+      });
+
+      expect(document.dispatchEvent).toHaveBeenCalledWith(mockArgument);
+    });
+  });
+
+  describe('EventEmitter.unsubscribe', () => {
+    it('should remove custom event listener from target', () => {
+      const eventEmitter = setupEventEmitter();
+      eventEmitter.unsubscribe(mockEventName, mockListener);
+      expect(document.removeEventListener).toHaveBeenCalled();
+    });
+  });
+});
---
+++ services/frontend/app/utils/event/index.ts
@@ -0,0 +1,57 @@
+import type {
+  Action,
+  EventLog,
+  EventName,
+  Listener,
+  Payload,
+  ServiceEventName,
+  ServiceName,
+  IEventEmitter,
+  IEventHelper,
+} from '~frontend/utils/event/types';
+
+export const createEventHelper = (serviceName: ServiceName): IEventHelper => {
+  const getServiceEventName = (eventName: EventName): ServiceEventName => {
+    return `${serviceName}:${eventName}`;
+  };
+
+  const getEventLog = (eventName: EventName, action: Action): EventLog => {
+    return {
+      eventName: `${eventName}:${action}`,
+      serviceName,
+    };
+  };
+
+  return { getServiceEventName, getEventLog };
+};
+
+export const createEventEmitter = (serviceName: ServiceName): IEventEmitter => {
+  const helper = createEventHelper(serviceName);
+  const { getServiceEventName } = helper;
+
+  const subscribe = (eventName: string, listener: Listener): void => {
+    const serviceEventName = getServiceEventName(eventName);
+    document.addEventListener(serviceEventName, (event: CustomEvent) => {
+      listener(event.detail);
+    });
+    console.info('Subscribe to browser event', {
+      detail: { eventName: serviceEventName },
+    });
+  };
+
+  const emit = (eventName: string, payload?: Payload): void => {
+    const serviceEventName = getServiceEventName(eventName);
+    const customEvent = new CustomEvent(serviceEventName, { detail: payload });
+    document.dispatchEvent(customEvent);
+    console.info('Emit a browser event', {
+      detail: { eventName: serviceEventName, payload },
+    });
+  };
+
+  const unsubscribe = (eventName: string, listener: Listener): void => {
+    const serviceEventName = getServiceEventName(eventName);
+    document.removeEventListener(serviceEventName, listener);
+  };
+
+  return { ...helper, subscribe, emit, unsubscribe };
+};
