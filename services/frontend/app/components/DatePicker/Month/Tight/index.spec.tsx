3490e227855bf0e73ba4b45f087bb09b18a07bb4
Chin <greeting.to.chin@gmail.com>
2023-09-16 23:39:23 +0800

    test(frontend): add datepicker month test case

---
+++ services/frontend/app/components/DatePicker/Month/Tight/index.spec.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { DAYS } from '~frontend/components/DatePicker/constants';
+import { getMonthDays } from '~frontend/components/DatePicker/Month/utils';
+import { DatePickerMonthTight } from '.';
+
+describe('DatePickerMonthTight', () => {
+  const onSelect = jest.fn();
+  const selectedDate = new Date('2023-01-28, 00:00:00');
+
+  it('renders the days of the week', () => {
+    render(
+      <DatePickerMonthTight onSelect={onSelect} selectedDate={selectedDate} />
+    );
+
+    DAYS.forEach((day) => {
+      expect(screen.getByText(day)).toBeInTheDocument();
+    });
+  });
+
+  it('should render the days of the month', () => {
+    render(
+      <DatePickerMonthTight onSelect={onSelect} selectedDate={selectedDate} />
+    );
+
+    const daysInMonth = getMonthDays(selectedDate);
+
+    daysInMonth.forEach(({ day }) => {
+      expect(screen.getAllByText(String(day))[0]).toBeInTheDocument();
+    });
+
+    expect(screen.getAllByRole('button')).toHaveLength(daysInMonth.length);
+  });
+
+  it('calls onSelect when a day is clicked', async () => {
+    render(
+      <DatePickerMonthTight onSelect={onSelect} selectedDate={selectedDate} />
+    );
+
+    const dayToClick = selectedDate.getDate();
+    await userEvent.click(
+      screen.getByRole('button', { name: String(dayToClick) })
+    );
+
+    expect(onSelect).toHaveBeenCalledWith(selectedDate);
+  });
+});
--- services/frontend/app/components/DatePicker/Month/utils/index.spec.ts
+++ services/frontend/app/components/DatePicker/Month/utils/index.spec.ts
@@ -1,17 +1,4 @@
-import { getMonthDays, getDateInfo } from '.';
-
-describe('getDateInfo', () => {
-  it('returns an object with the month, day, and weekday of a given date', () => {
-    const date = new Date('2022-02-01');
-    const expected = {
-      month: 2,
-      day: 1,
-      weekday: 2,
-    };
-
-    expect(getDateInfo(date)).toEqual(expected);
-  });
-});
+import { getMonthDays } from '.';
 
 describe('getMonthDays', () => {
   it('returns an array of objects representing the days in a month', () => {
