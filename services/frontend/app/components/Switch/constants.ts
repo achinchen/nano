f438e3d602b608ceca65fd22dd67c3df08002d80
Chin <greeting.to.chin@gmail.com>
2023-09-18 21:15:08 +0800

    chore(frontend): replace dark with zinc-900 or zinc-800

--- services/frontend/app/components/Switch/constants.ts
+++ services/frontend/app/components/Switch/constants.ts
@@ -1,6 +1,6 @@
 const CONTAINER_CLASS =
   'relative inline-block h-7 w-12 border-2 border-solid duration-100 border-rounded-3xl cursor-pointer';
-export const CONTAINER_CLASS_UNCHECKED = `${CONTAINER_CLASS} bg-dark-500 opacity-20 border-dark-500`;
+export const CONTAINER_CLASS_UNCHECKED = `${CONTAINER_CLASS} bg-zinc-900 opacity-20 border-zinc-900`;
 export const CONTAINER_CLASS_CHECKED = `${CONTAINER_CLASS} bg-primary-500 border-primary-500`;
 
 const BUTTON_CLASS =
