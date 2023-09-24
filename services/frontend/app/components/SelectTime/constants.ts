100baa465f98691c47fabdd8127465be9d8916cd
Chin <greeting.to.chin@gmail.com>
2023-09-24 20:14:23 +0800

    feat(frontend): add select time

---
+++ services/frontend/app/components/SelectTime/constants.ts
@@ -0,0 +1,14 @@
+const HOUR = Array.from({ length: 24 }, (_, i) =>
+  String(i).padStart(2, '0')
+).map((hour) => ({ label: hour, value: hour }));
+
+export const MINUTE = Array.from({ length: 6 }, (_, i) => String(i * 10)).map(
+  (minute) => ({ label: minute, value: minute })
+);
+
+export const UNITS = ['hour', 'minute'] as const;
+
+export const OPTION_BY_UNIT = {
+  hour: HOUR,
+  minute: MINUTE,
+};
