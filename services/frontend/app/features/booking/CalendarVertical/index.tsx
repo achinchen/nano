22459fc326d7d461a8f13862b9f62a2458587b14
Chin <greeting.to.chin@gmail.com>
2023-10-19 11:17:38 +0800

    feat: use pan replace drag

--- services/frontend/app/features/booking/CalendarVertical/index.tsx
+++ services/frontend/app/features/booking/CalendarVertical/index.tsx
@@ -1,9 +1,16 @@
-import { useEffect, useState } from 'react';
+import type { PanInfo } from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 import { useBookingContext } from '~frontend/features/booking/context';
 import { SheetIndicator } from '~frontend/components/Sheet';
 import { CalendarMonthTight } from '~frontend/components/Calendar/Month';
 import { CalendarWeek } from '~frontend/components/Calendar/Week';
+import {
+  getFirstDateInNextMonth,
+  getFirstDateInPreviousMonth,
+  getAfter,
+  getBefore,
+} from '~frontend/utils/date';
 import { useCalendarVerticalContext } from './context';
 
 const mockServiceData = {
@@ -21,10 +28,49 @@ const getMockData = (month: number) => {
   }, {});
 };
 
+const DAYS = 7;
+
+function toPrevious(isWeek: boolean) {
+  return isWeek
+    ? (date: Date) => getBefore(date, DAYS)
+    : getFirstDateInPreviousMonth;
+}
+
+function toNext(isWeek: boolean) {
+  return isWeek
+    ? (date: Date) => getAfter(date, DAYS)
+    : getFirstDateInNextMonth;
+}
+
 export function CalendarVertical({ className = '' }: { className?: string }) {
   const { mode, toggleMode } = useCalendarVerticalContext();
   const { selectedDate, setSelectedDate } = useBookingContext();
   const [serviceData, setServiceData] = useState({});
+  const [panInfo, setPanInfo] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const isWeek = mode === 'week';
+
+  const onPanStart = (_: unknown, { offset }: PanInfo) => {
+    setPanInfo({
+      x: offset.x,
+      y: offset.y,
+    });
+  };
+
+  const onPanEnd = (_: unknown, { offset }: PanInfo) => {
+    const isChangeMode = Math.abs(offset.y - panInfo.y);
+    if (isChangeMode) return toggleMode();
+
+    const isToPrevious = offset.x < panInfo.x;
+    setSelectedDate((selectedDate) =>
+      isToPrevious
+        ? toPrevious(isWeek)(selectedDate)
+        : toNext(isWeek)(selectedDate)
+    );
+  };
 
   useEffect(() => {
     const thisMonth = new Date().getMonth();
@@ -36,22 +82,19 @@ export function CalendarVertical({ className = '' }: { className?: string }) {
     <section className={className}>
       <motion.main
         className="flex flex-col rounded-b-4 bg-white px-4 shadow-default"
-        drag="y"
         layout
         transition={{ duration: 0 }}
         style={{ touchAction: 'none' }}
-        dragConstraints={{ top: 0, bottom: 0 }}
-        onDragStart={toggleMode}
-        // onScroll={onChange}
+        onPanStart={onPanStart}
+        onPanEnd={onPanEnd}
       >
-        {mode === 'week' && (
+        {isWeek ? (
           <CalendarWeek
             onSelect={setSelectedDate}
             selectedDate={selectedDate}
             data={serviceData}
           />
-        )}
-        {mode === 'month' && (
+        ) : (
           <CalendarMonthTight
             onSelect={setSelectedDate}
             selectedDate={selectedDate}
--- services/frontend/app/utils/date/index.ts
+++ services/frontend/app/utils/date/index.ts
@@ -1,3 +1,4 @@
+import type { ManipulateType } from 'dayjs';
 import zhTW from 'dayjs/locale/zh-tw';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import weekday from 'dayjs/plugin/weekday';
@@ -36,4 +37,20 @@ export const getFirstDateInPreviousMonth = (date: Date) => {
 
 export const getFirstDateInNextMonth = (date: Date) => {
   return dayjs(date).add(1, 'month').set('date', 1).toDate();
+};
+
+export const getBefore = (
+  date: Date,
+  day: number,
+  unit: ManipulateType = 'day'
+) => {
+  return dayjs(date).add(day, unit).toDate();
+};
+
+export const getAfter = (
+  date: Date,
+  day: number,
+  unit: ManipulateType = 'day'
+) => {
+  return dayjs(date).subtract(day, unit).toDate();
 };
