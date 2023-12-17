44d9c1596f6f3086cfcd5df64326620b7317d50b
Chin <greeting.to.chin@gmail.com>
2023-12-17 18:36:31 +0800

    chore: rename folder

--- services/frontend/src/features/my/setting/Header/index.tsx
+++
@@ -1,9 +0,0 @@
-import i from './i.json';
-
-export default function Header() {
-  return (
-    <header className="flex content-header">
-      <h2 className="text-lg md:text-xl">{i.title}</h2>
-    </header>
-  );
-}
